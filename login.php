ee2e7a812053b72db459a97b81b2220023c7af04
Ryan <ryanadiputra1919@gmail.com>
2026-06-05 15:23:13 +0700

    p

---
+++ login.php
@@ -0,0 +1,11 @@
+<form action="proses.login.php" method="POST">
+    <h2>Login Admin ambadrink</h2>
+    <label>Username:</label>
+    <input type="text" name="txt_user" required>
+
+    <label>Password:</label>
+    <input type="password" name="txt_pass" required>
+
+    <button type="submit" name="btn_login">Masuk</button>
+    
+</form>
