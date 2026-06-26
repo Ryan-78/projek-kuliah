0ccc1acd25d88b96a77ce1a4a5e245297ae456ee
Ray <yessiessi@gmail.com>
2026-06-26 13:24:08 +0700

    tambah

--- admin/tambah-produk.php
+++ admin/tambah-produk.php
@@ -10,4 +10,25 @@ if (!isset($_SESSION['id']) || $_SESSION['id'] != 12) {
 include "../koneksi.php";
 
 
+if (isset($_POST['simpan'])) {
+    // Ambil data dari form HTML
+    $nama      = $_POST['nama_produk'];
+    $deskripsi = $_POST['deskripsi'];
+    $harga     = $_POST['harga'];
+
+    
+    $query = "INSERT INTO produk (nama_produk, deskripsi, harga) VALUES ('$nama', '$deskripsi', '$harga')";
+    
+    
+    if (mysqli_query($koneksi, $query)) {
+        echo "<script>
+                alert('Produk berhasil ditambahkan!');
+                window.location='produk.php'; 
+              </script>";
+    } else {
+        echo "Gagal menyimpan: " . mysqli_error($koneksi);
+    }
+}
+
+
 ?>
